bae010b010ae2499770f0ce75c72057c9051f1ca
Ecialo <mightyecialo@gmail.com>
2016-03-02 10:04:52 +0300

    Добавлены ресуры и контекстные действия

--- core/card/card.py
+++ core/card/card.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import collections
 from .. import utility
 from .. import predef
 __author__ = 'Ecialo'
@@ -8,4 +9,33 @@ class Card(utility.Component):
 
     categories = [predef.CARD]
     description = ""
+    available_resources = []
+    available_actions = {}
+
+    def __init__(self):
+        self._current_context = None
+        self._components = collections.defaultdict(dict)
+
+        for resource in self.available_resources:
+            self.register_resource(resource)
+        super(Card, self).__init__()
+
+    @property
+    def resources(self):
+        return self._components[predef.RESOURCE]
+
+    @property
+    def associated_components(self):
+        return self.resources.values()
+
+    def register_resource(self, resource):
+        player_resource = resource()
+        player_resource.setup_prefix(self._name)
+        self._components[predef.RESOURCE][resource.name] = player_resource
+
+    def get_current_available_actions(self):
+        return map(
+            lambda action: action.setup(source=self),
+            self.available_actions[self._current_context.name]
+        )
 
