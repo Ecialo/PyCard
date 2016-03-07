5f402fbaf356ef0051098e57e46c396fa8289732
Ecialo <mightyecialo@gmail.com>
2016-03-07 22:37:28 +0300

    Добавлены действия по добавлению и избавлению от карт

--- core/action/common_system_actions/remove_cards.py
+++ core/action/common_system_actions/remove_cards.py
@@ -1,2 +1,15 @@
 # -*- coding: utf-8 -*-
+from .. import action
 __author__ = 'ecialo'
+
+
+class RemoveCards(action.Action):
+
+    def __init__(self, author, **kwargs):
+        self.target = None
+        self.cards = None
+        super(RemoveCards, self).__init__(author, **kwargs)
+
+    def apply(self):
+        for card in self.cards:
+            self.target.remove_card(card)
--- core/card/hand.py
+++ core/card/hand.py
@@ -14,6 +14,7 @@ class Hand(utility.Component):
     def __init__(self):
         super(Hand, self).__init__()
         self._cards = []
+        self._num_of_cards = 0
         self._selected_cards = []
 
     def select_cards(self, *cards):
@@ -21,6 +22,12 @@ class Hand(utility.Component):
 
     def get_cards(self, *cards):
         self._cards += cards
+        self._num_of_cards += len(cards)
 
     def make_action(self, action):
         pass
+
+    def remove_card(self, card):
+        if card:
+            self._cards.remove(card)
+        self._num_of_cards += 1
