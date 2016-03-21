7b684cf78f64137220597bfe511c356b309037f0
Ecialo <mightyecialo@gmail.com>
2016-03-21 21:22:42 +0300

    Заготовки для действий

---
+++ sample_games/durak/durak_actions.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from core.action import action
+__author__ = 'ecialo'
+
+
+class PlayAsAttack(action.Action):
+    pass
+
+
+class PlayAsDefend(action.Action):
+    pass
+
+
+class Grab(action.Action):
+    pass
