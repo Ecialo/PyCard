b86baa03d626531f5a5d688e34d9ad376ef346c2
eveningsteps <eveningsteps@gmail.com>
2016-04-07 21:41:31 +0300

    scrollable hand widget

--- client/game_ui/hand_widget/hand_widget.py
+++ client/game_ui/hand_widget/hand_widget.py
@@ -6,6 +6,7 @@ import kivy.uix.floatlayout as flayout
 import kivy.uix.anchorlayout as alayout
 import kivy.uix.relativelayout as rlayout
 import kivy.uix.stacklayout as slayout
+import kivy.uix.boxlayout as blayout
 import kivy.uix.widget as widget
 import kivy.uix.label as label
 import kivy.uix.button as button
@@ -14,23 +15,22 @@ import kivy.properties as prop
 from kivy.lang import Builder
 
 
-class HandWidget(slayout.StackLayout):
+class HandWidget(blayout.BoxLayout):
 
     hand = prop.ObjectProperty()
     cards = prop.ListProperty()
-
+    
     def __init__(self, hand, **kwargs):
         super(HandWidget, self).__init__(**kwargs)
-        self.ids = kwargs.get('ids')
+        #self.ids = kwargs.get('ids')
         self.hand = hand
+        
         self.register_event_type('on_get_cards')
 
     def on_get_cards(self, *cards):
-        # print "\n\n\nIDS\n", self.ids.game
         for card in cards:
             card = self.ids.game.game.view_card(card)
             card_widget = card.make_widget()
-            self.add_widget(widget=card_widget)
-
+            self.ids.card_stack.add_widget(widget=card_widget)
 
 Builder.load_file('./hand_widget/hand_widget.kv')
