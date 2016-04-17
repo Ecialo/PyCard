768100e87c78e119e699684b01ed090a75a238fc
BRagnar <korobq@gmail.com>
2016-04-17 15:12:08 +0300

    Major refactoring + game init from lobby

--- server/player/player_class.py
+++ server/player/player_class.py
@@ -12,15 +12,14 @@ class LobbyPerson(object):
     Подсоединяющийся к pycard_server клиент
     """
 
-    def __init__(self, identifier, name):
+    def __init__(self, name):
         """
         На входе принимает те параметры с которыми прилетает коннект
         к серверу от клиента, а именно имя и идентификатор;
         также у игрока есть статус готов/не готов
         """
 
-        self.name = name
-        self.identifier = identifier
+        self._name = name
         self.ready = False
 
     def get_ready(self):
@@ -35,16 +34,6 @@ class LobbyPerson(object):
         """
         self.ready = False
 
-    def get_person_data(self):
-        """
-        Вызвать когда нужно в стороннее место
-        скормить аттрибуты этого класса как словарь
-        пока возвращает только нужные поля для retard game
-        """
-
-        # TODO: fix this hack
-        return {'name': self.identifier}
-
     @property
-    def name_getter(self):
-        return self.name
+    def name(self):
+        return self._name
