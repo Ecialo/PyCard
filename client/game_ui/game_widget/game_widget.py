5d072d654b4cd4146781ae6990d870c0d9641f3d
eveningsteps <eveningsteps@gmail.com>
2016-04-18 16:42:13 +0300

    fix stray conflict markers

--- client/game_ui/game_widget/game_widget.py
+++ client/game_ui/game_widget/game_widget.py
@@ -4,15 +4,11 @@ import kivy.uix.floatlayout as flayout
 from kivy.uix import tabbedpanel
 import kivy.properties as prop
 from kivy.lang import Builder
-<<<<<<< HEAD
 from kivy.uix.screenmanager import Screen
-=======
->>>>>>> a60ce7fe9ae159fade89c526593a81cf0f7e9a23
 
 from core import predef
 
 __author__ = 'ecialo'
-<<<<<<< HEAD
 Builder.load_file('./client/game_ui/game_widget/game_widget.kv')
 
 class GameWidget(Screen):
@@ -25,19 +21,10 @@ class GameWidget(Screen):
     deck_widget = prop.ObjectProperty()
 
     pending_actions = []
-=======
-Builder.load_file('./game_widget/game_widget.kv')
-
-
-class GameWidget(flayout.FloatLayout):
-
-    game = prop.ObjectProperty()
->>>>>>> a60ce7fe9ae159fade89c526593a81cf0f7e9a23
 
     def __init__(self, game, **kwargs):
         super(GameWidget, self).__init__(**kwargs)
         self.game = game
-<<<<<<< HEAD
         self.player_name = self.app.player_name
 
         self.player_widgets = {player.name: player.make_widget(game_widget=self) for player in self.game.get_category(predef.PLAYER).itervalues()}
@@ -83,21 +70,3 @@ class GameWidget(flayout.FloatLayout):
     def notify(self, text):
         print(text) # TODO: короткие попапы?
 
-=======
-        player_widgets = [player.make_widget(game_widget=self) for player in self.game.get_category(predef.PLAYER).itervalues()]
-        player_zone = self.ids.player_zone
-
-        for widget in player_widgets:
-            tab = tabbedpanel.TabbedPanelItem(text=widget.player.name)
-            tab.add_widget(widget=widget)
-            player_zone.add_widget(tab)
-
-        #desk_widgets = [desk.make_widget() for desk in self.game.get_category(predef.DESK).itervalues()]
-        #for w in desk_widgets:
-        #    main_zone.add_widget(w)
-
-        deck_zone = self.ids.deck_zone
-        deck_widgets = [deck.make_widget(game_widget=self) for deck in self.game.get_category(predef.DECK).itervalues()]
-        for w in deck_widgets:
-            deck_zone.add_widget(w)
->>>>>>> a60ce7fe9ae159fade89c526593a81cf0f7e9a23
