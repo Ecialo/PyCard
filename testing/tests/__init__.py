1d8483c6476d67e6cb7104a00a78aed52aeebc40
Ecialo <mightyecialo@gmail.com>
2016-04-23 20:08:50 +0300

    protocol_checker

--- testing/tests/__init__.py
+++ testing/tests/__init__.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 import unittest
 import retard_game
+import protocol_checker
 __author__ = 'ecialo'
 
 test_suit = unittest.TestSuite(
-    [unittest.makeSuite(retard_game.TestRetardGame)]
+    [
+        unittest.makeSuite(retard_game.TestRetardGame),
+        unittest.makeSuite(protocol_checker.TestProtocolChecker),
+    ]
 )
