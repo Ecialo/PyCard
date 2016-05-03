f02902274f966431a90ee1aaa4e68865c7b93e49
eveningsteps <eveningsteps@gmail.com>
2016-05-03 11:46:18 +0300

    add ability to view last N runs (useful when clients are running simultaneously)

--- tools/filter_logs.py
+++ tools/filter_logs.py
@@ -8,19 +8,24 @@ import argparse
 from twisted.logger import eventsFromJSONLogFile, textFileLogObserver
 
 
-def print_log(fp, name=None, last_only=True, dest=sys.stdout):
+def print_log(fp, user=None, n=1, dest=sys.stdout):
     output = textFileLogObserver(dest)
-    events = []
+    events, startups = [], []
 
-    last_start_time = 0 if not last_only else 1e64 # should get us covered till the end of time (literally)
     for event in eventsFromJSONLogFile(io.open(fp)):
         events.append(event)
         if event.get('log_format') == 'Start client':
-            last_start_time = event.get('log_time')
+            startups.append(event.get('log_time'))
+
+    last_start_time = 0
+    if len(startups) >= n:
+        last_start_time = startups[-n]
+    elif startups:
+        last_start_time = startups[0]
 
     for event in events:
-        fitting_timestamp = (not last_only or last_start_time <= event.get('log_time'))
-        fitting_name = (not name or name == event.get('player_name'))
+        fitting_timestamp = (last_start_time <= event.get('log_time'))
+        fitting_name = (not user or user == event.get('player_name'))
 
         if fitting_timestamp and fitting_name:
             output(event)
@@ -28,11 +33,11 @@ def print_log(fp, name=None, last_only=True, dest=sys.stdout):
 def main():
     parser = argparse.ArgumentParser(description='Prettify logs produced by twisted.logger.jsonFileLogObserver')
     parser.add_argument('-f', '--file', dest='path', help='Path to log file', required=True)
-    parser.add_argument('-a', '--all', dest='last_only', action='store_false', help='Print the whole log. By default, only last run is printed')
-    parser.add_argument('-n', '--name', dest='name', help='Name of the user whose logs you want to read')
+    parser.add_argument('-n', '--launches', dest='n', type=int, default=1, help='Print last N client runs. By default, only the most recent run is printed')
+    parser.add_argument('-u', '--user', dest='user', help='Name of the user whose logs you want to read')
 
     args = parser.parse_args()
-    print_log(fp=args.path, name=args.name, last_only=args.last_only)
+    print_log(fp=args.path, user=args.user, n=args.n)
 
 
 if __name__ == '__main__':
